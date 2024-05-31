2da08559d409f2b569a1fb38a4f8678848ea95df
Geovanni Izidoro <geovanniizi2002@hotmail.com>
2024-05-30 22:08:20 -0300

    first commit

---
+++ EmprestimosDeLivros/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmprestimosDeLivros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimosDeLivros.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {   
+        }
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+
+    }
+}
---
+++ EmprestimosDeLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace EmprestimosDeLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Digite o nome do Recebedor!")]
+        public string Recebedor { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do Fornecedor!")]
+        public string Fornecedor { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do Livro!")]
+        public String LivroEmprestado { get; set; }
+        public DateTime dataUltimaAtualizacao { get; set; }
+    }
+}
